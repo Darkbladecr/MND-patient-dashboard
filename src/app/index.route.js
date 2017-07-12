4875a1879b782c5d74762f08eb01afe1e8115e8d
Darkbladecr <smig88@gmail.com>
2017-07-12 10:57:01 +0100

    Changed to horizontal navigation panel

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,6 +1,10 @@
-import verticalNavigationTemplate from './core/layouts/vertical-navigation.html';
-import toolbarTemplate from './toolbar/layouts/vertical-navigation/toolbar.html';
-import navigationTemplate from './navigation/layouts/vertical-navigation/navigation.html';
+// import verticalNavigationTemplate from './core/layouts/vertical-navigation.html';
+// import toolbarTemplate from './toolbar/layouts/vertical-navigation/toolbar.html';
+// import navigationTemplate from './navigation/layouts/vertical-navigation/navigation.html';
+import contentToolbarTemplate from './core/layouts/content-with-toolbar.html';
+import horzToolbarTemplate from './toolbar/layouts/content-with-toolbar/toolbar.html';
+import horzNavigationTemplate from './navigation/layouts/horizontal-navigation/navigation.html';
+
 import quickPanelTemplate from './quick-panel/quick-panel.html';
 
 function routeConfig($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -12,27 +16,26 @@ function routeConfig($stateProvider, $urlRouterProvider, $locationProvider) {
 	$urlRouterProvider.otherwise('/pages/auth/login');
 
 	// State definitions
-	$stateProvider
-		.state('app', {
-			abstract: true,
-			views: {
-				'main@': {
-					template: verticalNavigationTemplate,
-					controller: 'MainController as vm'
-				},
-				'toolbar@app': {
-					template: toolbarTemplate,
-					controller: 'ToolbarController as vm'
-				},
-				'navigation@app': {
-					template: navigationTemplate,
-					controller: 'NavigationController as vm'
-				},
-				'quickPanel@app': {
-					template: quickPanelTemplate,
-					controller: 'QuickPanelController as vm'
-				}
-			}
-		});
+	$stateProvider.state('app', {
+		abstract: true,
+		views: {
+			'main@': {
+				template: contentToolbarTemplate,
+				controller: 'MainController as vm',
+			},
+			'toolbar@app': {
+				template: horzToolbarTemplate,
+				controller: 'ToolbarController as vm',
+			},
+			'navigation@app': {
+				template: horzNavigationTemplate,
+				controller: 'NavigationController as vm',
+			},
+			'quickPanel@app': {
+				template: quickPanelTemplate,
+				controller: 'QuickPanelController as vm',
+			},
+		},
+	});
 }
 export default routeConfig;
