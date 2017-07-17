f9e349a9e83e751daf49448b75c4d9d846dbc605
Darkbladecr <smig88@gmail.com>
2017-07-17 09:54:03 +0100

    Hack to get profile picture to work on build

--- main.js
+++ main.js
@@ -9,7 +9,12 @@ let win;
 
 function createWindow() {
 	// Create the browser window.
-	win = new BrowserWindow({ width: 1000, height: 1000 });
+	win = new BrowserWindow({
+		width: 1000,
+		height: 800,
+		minWidth: 620,
+		minHeight: 450,
+	});
 
 	// and load the index.html of the app.
 	win.loadURL(
